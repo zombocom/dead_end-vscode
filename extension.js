2c0fb5fe391713a0a2258594c1fb102a71095c34
Mauro Otonelli <mauro.otonelli@gmail.com>
2021-02-03 02:37:27 -0300

    Basic functionality done! TODO: actual diagnostic ranges, enable/disable ext

--- extension.js
+++ extension.js
@@ -1,16 +1,55 @@
 const vscode = require('vscode');
+const cp = require('child_process');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('Congratulations, your extension "dead-end-vscode" is now active!');
+	const diagnosticCollection = vscode.languages.createDiagnosticCollection('ruby');
+	console.log('Extension "dead-end-vscode" is now active!');
 
-	let disposable = vscode.commands.registerCommand('dead-end-vscode.helloWorld', function () {
-		vscode.window.showInformationMessage('Hello World from dead_end!');
+	vscode.workspace.onDidSaveTextDocument(function (document) {
+		if(document.languageId == "ruby") {
+			diagnosticCollection.clear();
+			vscode.window.showInformationMessage('Saved file, running dead_end');
+
+			cp.exec("dead_end --no-terminal " + document.fileName, (_err, _stdout, stderr) => {
+				console.log(stderr);
+				if (stderr == "Syntax OK") return;
+
+				const lineRegex = /❯ (\d+)/g;
+				const allLines = [];
+				let m;
+
+				while ((m = lineRegex.exec(stderr)) !== null) {
+					if (m.index === lineRegex.lastIndex) {
+        		lineRegex.lastIndex++;
+    			}
+
+					allLines.push(parseInt(m[1]));
+				}
+
+				// Probably should programatically get first and last column, the regex
+				// could be improved to include the actual code after the line number,
+				// so we can get the line lengths.
+				//
+				// We might need to change the data structure of the allLines var to be
+				// a proper object instead, to store the first line number, its length,
+				// and the last line number and its length.
+				const range = new vscode.Range(
+					allLines[0] - 1,
+					0,
+					allLines[allLines.length - 1],
+					120
+				);
+
+				const diagnostic = new vscode.Diagnostic(range, stderr, vscode.DiagnosticSeverity.Error);
+				diagnosticCollection.set(document.uri, [diagnostic]);
+			});
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(diagnosticCollection);
 }
 
 function deactivate() {}
