0d9ab389a80ed9ee8c7bd2736941e04cc6368143
Mauro Otonelli <mauro.otonelli@gmail.com>
2021-02-09 00:14:52 -0300

    Use spaces everywhere

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -7,27 +7,27 @@ chai.use(require('sinon-chai'));
 const expect = chai.expect;
 
 suite('Extension Test Suite', () => {
-	const document = {
-		languageId: 'ruby'
-	};
-	const context  = {
-		subscriptions: [],
-	};
-	const sandbox = sinon.createSandbox();
+  const document = {
+    languageId: 'ruby'
+  };
+  const context  = {
+    subscriptions: [],
+  };
+  const sandbox = sinon.createSandbox();
 
-	test('Extension activation', () => {
-		const consoleLog = sandbox.stub(console, 'log');
+  test('Extension activation', () => {
+    const consoleLog = sandbox.stub(console, 'log');
 
-		activate(context);
+    activate(context);
 
-		expect(consoleLog).calledOnceWith('Extension "dead_end-vscode" is now active!');
-	});
+    expect(consoleLog).calledOnceWith('Extension "dead_end-vscode" is now active!');
+  });
 
-	test('Diagnostics creation', () => {
-		const diagnosticCollection = sandbox.stub(vscode.languages, 'createDiagnosticCollection');
+  test('Diagnostics creation', () => {
+    const diagnosticCollection = sandbox.stub(vscode.languages, 'createDiagnosticCollection');
 
-		activate(context);
+    activate(context);
 
-		expect(diagnosticCollection).calledOnceWith('ruby');
-	});
+    expect(diagnosticCollection).calledOnceWith('ruby');
+  });
 });
